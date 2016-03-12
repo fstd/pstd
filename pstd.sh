e45269d1b36c7254e0ba70a6de38fe93205b1168
fstd <van.fstd@gmail.com>
2016-03-12 05:38:30 +0100

    pstd.sh: add -d to download pastes

--- pstd.sh
+++ pstd.sh
@@ -11,7 +11,10 @@ version='0.0.2'
 Usage() # Print usage statement and exit
 {
 	printf 'Usage %s [-h] [-s <pastesite>] [<file>]\n' "$prgnam" >&2
+	printf 'or    %s [-h] [-s <pastesite>] -d <paste_id>\n' "$prgnam" >&2
 	printf '  If <file> is absent or `-`, we paste stdin.\n' >&2
+	printf '  If -d <paste_id> is given, *down*load the respective\n' >&2
+	printf '    paste instead and output it on stdout\n' >&2
 	printf '  The default paste site is `%s`.\n\n' "$site" >&2
 	printf 'v%s, 2015, Timo Buhrmester\n' "$version" >&2
 	exit 1
@@ -38,30 +41,38 @@ tmpout="$(mktemp /tmp/paste.XXXXXXXX)"
 trap 'rm -f "$tmpin" "$tmpout"' EXIT
 
 
+dl=false
 # Parse command line arguments
-while getopts "s:h" i; do
+while getopts "s:dh" i; do
 	case "$i" in
 	s) site="$OPTARG" ;;
+	d) dl=true ;;
 	*) Usage ;;
 	esac
 done
 shift $((OPTIND-1))
 
-# Warn about superfluous argumnents
-[ $# -gt 1 ] && printf '%s: Ignoring all but the first argument\n' "$prgnam" >&2
+if $dl; then
+	[ $# -gt 0 ] || Bomb "No paste identifier given"
+	wget -q -O - "http://$site/$1" >"$tmpout" || Bomb "wget failed :O"
 
-# If a file is given on the command line, paste that. Else, paste standard input.
-if [ $# -gt 0 -a "$1" != '-' ]; then
-	[ -r "$1" ] || Bomb "Cannot read '$1'"
-
-	in="$1"
 else
-	cat >"$tmpin"
-	in="$tmpin"
-fi
+	# Warn about superfluous argumnents
+	[ $# -gt 1 ] && printf '%s: Ignoring all but the first argument\n' "$prgnam" >&2
 
-# Paste what $in refers to
-wget -q -O - --post-file "$in" "http://$site" >"$tmpout" || Bomb "wget failed :O"
+	# If a file is given on the command line, paste that. Else, paste standard input.
+	if [ $# -gt 0 -a "$1" != '-' ]; then
+		[ -r "$1" ] || Bomb "Cannot read '$1'"
+
+		in="$1"
+	else
+		cat >"$tmpin"
+		in="$tmpin"
+	fi
+
+	# Paste what $in refers to
+	wget -q -O - --post-file "$in" "http://$site" >"$tmpout" || Bomb "wget failed :O"
+fi
 
 # And output the returned link (or error)
 cat "$tmpout"
